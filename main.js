dd6d3d973cec781930d9dc3c4378baccce72a2b3
devenock <enockomondi305@gmail.com>
2020-12-04 17:26:55 +0300

    adding the email

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ function showRegisterPage(){
     document.location = 'register.html';
 }
 
+
+// submit button
+
+const submitButton = document.getElementById('button');
+const emailInput = document.getElementById('email');
+submitButton.addEventListener('click', clearFields);
+
+function clearFields(e){
+    if(true){
+        e.preventDefault();
+        emailInput.value = "";
+    }
+}
+
